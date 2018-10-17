1b62093fe5454a59808a853e1570f0bc1779715a
Szegerege12 <mrszegerege@gmail.com>
2018-10-17 17:08:53 +0200

    learn

--- Day_3/Zadanie_1.py
+++ Day_3/Zadanie_1.py
@@ -6,9 +6,16 @@ jest podzielna przez 3, 5, 7
 Wypisz wyniki na ekran.
 
 Pamiętaj o komentarzach.
-
 Rezultat wypisz na ekran.
 
 Podpowiedź:
 Odpowiednio formatuj stringi
 """
+### wprowadzanie liczby pamietac o int.input!!!
+liczba = int(input('Wprowadz liczbe: '))
+
+##sprawdzanie warunków dla 3, 5 ,7
+if liczba%3==0:
+    print("Wprowadzona liczba jest podzielna przez '3'")
+else:
+    print("Wprowadzona liczba jest niepodzielna przez 3")
