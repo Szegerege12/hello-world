0a7da66128f9f926e2e1ba34d6288d4d39ea773b
Szegerege12 <mrszegerege@gmail.com>
2018-10-17 22:33:51 +0200

    z3

--- Day_3/Zadanie_4.py
+++ Day_3/Zadanie_4.py
@@ -7,7 +7,14 @@ Podpowiedź:
 jest podzielny przez 4, ale nie jest podzielny przez 100
 jest podzielny przez 400
 """
+### zapytanie o rok
+rok = int(input("Podaj rok: "))
 
+### sprawdzanie warunkow
+if (rok % 4 == 0) and (rok % 100 !=0) or (rok % 400 == 0):
+    print("Rok",rok,"jest rokiem przestepnym")
+else:
+    print("Rok",rok, "nie jest rokiem przestepnym")
 
 
 
