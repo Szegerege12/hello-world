785b48600d6899f2488606fca45d197aa5799280
Szegerege12 <mrszegerege@gmail.com>
2018-10-17 16:35:52 +0200

    asd

--- Day_2/Zadanie_1.py
+++ Day_2/Zadanie_1.py
@@ -10,7 +10,28 @@ Nie wolno dostarczać znaków z zewnątrz
 Podpowiedzi:
 https://pl.wikipedia.org/wiki/Prawa_De_Morgana
 """
+# De Morgan law:
+A = True
+B = True
+x = (not(A and B)) == ((not A) or (not B))
+y = (not(A or B)) == ((not A) and (not B))
+z = (A or B) == (not(not A) and (not B))
+print("Pierwsze prawo DeMorgana:")
+print(x)
+print("Drugiee prawo DeMorgana:")
+print(y)
+print("Alternatywa:")
+print(z)
+
+#tabele wartosci logicznych
+print("Tabela AND")
 print(True and True)
 print(True and False)
 print(False and True)
+print(False and False)
+
+print("Tabela OR")
+print(True or True)
+print(True or False)
+print(False or True)
 print(False and False)
--- Day_2/Zadanie_2.py
+++ Day_2/Zadanie_2.py
@@ -9,3 +9,17 @@ przy użyciu funkcji ord oraz chr
 Podpowiedzi:
 https://pl.wikipedia.org/wiki/ASCII
 """
+i1 = ord('M')
+i2 = ord('A')
+i3 = ord('R')
+i4 = ord('E')
+i5 = ord('K')
+n1 = ord('S')
+n2 = ord('I')
+n3 = ord('E')
+n4 = ord('N')
+n5 = ord('K')
+n6 = ord('O')
+print(i1,i2,i3,i4,i5, n1,n2,n3,n4,n5,n6)
+
+
