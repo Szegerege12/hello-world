bf55ea6fbc25c5b61b2acb9d3a78dad02ae74630
Szegerege12 <mrszegerege@gmail.com>
2018-10-18 11:20:20 +0200

    z5 d

--- Day_3/Zadanie_5.py
+++ Day_3/Zadanie_5.py
@@ -6,3 +6,10 @@ Pobierz od użytkownika rok.
 Wskaż liczbę dni w miesiącu.
 Rezultat wypisz na ekran.
 """
+
+### pobieranie nazwy miesiaca i roku
+month = int(input("Number of month: "))
+year = int(input("Year: "))
+from calendar import monthrange
+wynik = monthrange(year,month)
+print(wynik)
