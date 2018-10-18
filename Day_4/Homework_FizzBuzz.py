8c439d56266007fca51bdd982a09a69927ba1851
Szegerege12 <mrszegerege@gmail.com>
2018-10-18 18:48:10 +0200

    start

---
+++ Day_4/Homework_FizzBuzz.py
@@ -0,0 +1,11 @@
+"""
+Wymagania:
+Biorąc pod uwagę liczby od 1 do 100(włącznie)
+napisz "Fizz" jeśli liczba jest podzielna przez 3
+napisz "Buzz" jeśli liczba jest podzielna przez 5
+napisz "FizzBuzz" jeśli liczba jest podzielna przez 3 i przez 5
+jeśli nie spełnia żadnego z tych warunków wypisz liczbę.
+
+1 - użyj pętli while
+2 - użyj pętli for
+"""
---
+++ Day_4/Homework_Mario_Pyramid.py
@@ -0,0 +1,15 @@
+"""
+Wymagania:
+Narysuj na ekranie piramidę Mario
+(wypisując odpowiednio znaki #)
+jako input podaj wysokość piramidy
+
+Piramida wysokości 3 ma wyglądać:
+
+  #
+ ###
+#####
+
+1 - użyj jedynie pętli i znaków "#" oraz spacji
+2 - przeczytaj https://docs.python.org/3/library/string.html aby ułatwić sobie życie
+"""
