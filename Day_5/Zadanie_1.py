2c625b953cd28ada7258a650a8dd4906851d8423
Szegerege12 <mrszegerege@gmail.com>
2018-10-20 16:27:23 +0200

    done

--- Day_5/Zadanie_1.py
+++ Day_5/Zadanie_1.py
@@ -9,3 +9,15 @@ przypisz go do nowej zmiennej i wypisz na ekran.
 Podpowiedź:
 Skorzystaj z modułu random
 """
+### deklaracja tabeli i wyswietlenie w kolejnosci
+kolory = ['czarny','bialy','czerwony','niebieski','zielony']
+print(kolory)
+
+### import modulu random
+import random
+##import funkcji choice z modulu random
+from random import choice
+
+### przypisanie nowej zmiennej
+losowy = (random.choice(kolory))
+print("Losowy kolor z listy to:",losowy,)
--- Day_5/Zadanie_2.py
+++ Day_5/Zadanie_2.py
@@ -5,3 +5,12 @@ i dodaje je do listy dopóki użytkownik nie poda wartości 'nie'
 
 Wypisz listę na ekran.
 """
+
+lista = []
+while True:
+    n = input("Podaj wartosc: ")
+    lista.append(n)
+    if  n == ('nie'):
+        break
+
+print(lista)
