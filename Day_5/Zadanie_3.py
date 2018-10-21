e57c6190923fa663e4d0253c3eef5b15e1929bec
Szegerege12 <mrszegerege@gmail.com>
2018-10-21 14:15:04 +0200

    3

--- Day_5/Zadanie_3.py
+++ Day_5/Zadanie_3.py
@@ -11,3 +11,16 @@ Wypisz elementy słownika na ekran w formie "klucz -> wartość"
 Podpowiedź:
 Użyj dwóch inputów do pobrania wartości
 """
+### pobieranie informacji od uzytkownika
+
+slownik = {}
+while True:
+    imie =input("Prosze podac nazwisko wlasciciela pojazdu[nacisnij q zeby wyjsc): ")
+    if imie == 'q':
+        break
+    else:
+        pojazd =input("Prosze podac marke samochodu: ")
+        slownik[imie]=pojazd
+
+for key, value in slownik.items():
+    print(key," = ",value)
