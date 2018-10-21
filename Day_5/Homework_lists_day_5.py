6c8a3a385620dd8916194abf8c740ad8504feb0c
Szegerege12 <mrszegerege@gmail.com>
2018-10-21 16:41:50 +0200

    update

--- Day_5/Homework_lists_day_5.py
+++ Day_5/Homework_lists_day_5.py
@@ -13,3 +13,26 @@ zapyta co chcemy zrobić :
 
 Każdą wykonaną opcję proszę opatrzyć stosownym komunikatem
 """
+lista = []
+print("Witaj w programie. Jesli chcesz zakonczyć jego działanie kliknij klawisz 'q'\n")
+
+while True:
+    imie = input(str("Podaj imie: "))
+    if imie == 'q':
+        break
+    else:
+        lista.append(imie)
+
+print("Aby sprawdzić ile elementow jest w liscie nacisnij przycisk 'E'\n"
+      "Aby wypisac imiona z listy kliknij przycisk 'P'\n"
+      "Aby dodac imie do listy kliknij 'd'")
+decyzja = str(input("Co chcesz teraz zrobic: "))
+if decyzja == 'e':
+    licznik = len(lista)
+    print("Aktualnie na liscie jest:",licznik,"pozycji")
+elif decyzja == 'p':
+    print("Twoja lista imion to:\n",lista,)
+elif decyzja == 'd':
+    dodanie = str(input("Podaj imie ktore chcesz dodac do listy: "))
+    lista.append(dodanie)
+    print(lista)
--- Day_5/Zadanie_4.py
+++ Day_5/Zadanie_4.py
@@ -11,3 +11,11 @@ https://docs.python.org/3.6/tutorial/datastructures.html#list-comprehensions
 Wynik:
 [[0, 1, 2], [1, 2, 3], ...
 """
+def function(elements=100, lineLen=3):
+    numbers = [str(x) for x in range(elements)]
+    chunks = [numbers[x:x+lineLen] for x in range(0, len(numbers), lineLen)]
+    strContent = "\n".join([" ".join(item) for item in chunks])
+    return strContent
+
+some01 = function(elements=100, lineLen=3)
+print(some01)
