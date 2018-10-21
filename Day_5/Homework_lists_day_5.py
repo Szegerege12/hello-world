90cf0a01ba78eef4bfa6d3ed7133f10c151e05e6
Szegerege12 <mrszegerege@gmail.com>
2018-10-21 18:51:44 +0200

    v 1.0.0

--- Day_5/Homework_lists_day_5.py
+++ Day_5/Homework_lists_day_5.py
@@ -13,26 +13,39 @@ zapyta co chcemy zrobić :
 
 Każdą wykonaną opcję proszę opatrzyć stosownym komunikatem
 """
+### Deklaracja pustej listy
 lista = []
 print("Witaj w programie. Jesli chcesz zakonczyć jego działanie kliknij klawisz 'q'\n")
-
+### infinite loop dla wczytywania imienia chyba ze q
 while True:
     imie = input(str("Podaj imie: "))
     if imie == 'q':
         break
     else:
         lista.append(imie)
-
+### menu programu###
 print("Aby sprawdzić ile elementow jest w liscie nacisnij przycisk 'E'\n"
       "Aby wypisac imiona z listy kliknij przycisk 'P'\n"
-      "Aby dodac imie do listy kliknij 'd'")
-decyzja = str(input("Co chcesz teraz zrobic: "))
-if decyzja == 'e':
-    licznik = len(lista)
-    print("Aktualnie na liscie jest:",licznik,"pozycji")
-elif decyzja == 'p':
-    print("Twoja lista imion to:\n",lista,)
-elif decyzja == 'd':
-    dodanie = str(input("Podaj imie ktore chcesz dodac do listy: "))
-    lista.append(dodanie)
-    print(lista)
+      "Aby dodac imie do listy kliknij 'd'\n"
+      "Aby usunac imie z listy kliknij 'r'")
+
+### infinite loop dla zapytania co chcesz teraz zrobic(robi jeden krok po czym wraca do menu)###
+while True:
+    decyzja = str(input("Co chcesz teraz zrobic(aby zakończyć działanie programu kliknij q: "))
+    if decyzja == 'q':
+        break
+
+### wybory poszczegolnych klawiszy i co powoduja
+    elif decyzja == 'e':
+        licznik = len(lista)
+        print("Aktualnie na liscie jest:",licznik,"pozycji")
+    elif decyzja == 'p':
+        print("Twoja lista imion to:\n",lista,)
+    elif decyzja == 'd':
+        dodanie = str(input("Podaj imie ktore chcesz dodac do listy: "))
+        lista.append(dodanie)
+        print(lista)
+    elif decyzja == 'r':
+        usuwanie = str(input("Podaj imie ktore chcesz usunac: "))
+        lista.remove(usuwanie)
+        print(lista)
