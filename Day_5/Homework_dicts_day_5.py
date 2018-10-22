1cc5ff91789937e5ff76a1c3693ac6d3fee9d58e
Szegerege12 <mrszegerege@gmail.com>
2018-10-22 14:49:01 +0200

    d5 done

--- Day_5/Homework_dicts_day_5.py
+++ Day_5/Homework_dicts_day_5.py
@@ -18,3 +18,37 @@ Pomyśl:
 Jak wykorzystać słownik, co zrobić gdy podane imię już istnieje?
 Jak policzyć liczbę imion w słowniku?
 """
+### podawanie imienia
+name = ()
+slownik = {}
+while True:
+    print("-"*10)
+    name = input(str("Prosze podac imie, lub 'q' aby wyjsc: "))
+    print("-" * 10)
+    if name == 'q':
+        break
+    else:
+        nr = input("Prosze podac nr: ")
+        slownik[name] = nr
+    for key,val in slownik.items():
+         print(key,'=',val)
+
+print("-"*10)
+wybor = str(input("Nacisnij: \n"
+                  "C - by sprawdzic czy imie jest w slowniku\n"
+                  "R - by usunac imie ze slownika\n"
+                  "L - by sprawdzic liczbe imion w slowniku\n"))
+if wybor == 'c':
+    szukaj = input("Jakie imie chcesz wyszukac: ?")
+    if szukaj in slownik:
+            print("Imie wystepuje")
+    else:
+            print("Podane imie nie wystepuje w slowniku")
+elif wybor == 'r':
+    usun = input("Jakie imie chcesz usunac ze slownika?: ")
+    del slownik[usun]
+    print("Imie",usun,"zostalo usuniete ze slownika")
+    print(slownik)
+elif wybor == 'l':
+    liczenie = (len(slownik))
+    print("Liczba imion w słowniku to:",liczenie,)
