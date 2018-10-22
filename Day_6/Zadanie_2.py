f17a38730abbbf35bf0a461652965c89311a6e9d
Szegerege12 <mrszegerege@gmail.com>
2018-10-22 21:12:17 +0200

    done

--- Day_6/Zadanie_2.py
+++ Day_6/Zadanie_2.py
@@ -8,17 +8,39 @@ Napisz funkcję, sprawdzającą czy dana liczba jest w podanym zakresie liczb.
 Nie zapomnij o dokumentacji
 Przygotuj zestaw danych testowych
 """
+def zdanie(boolean):
+    """
+    Funkcja wyswietlajaca odpowiednie zdanie w zaleznosci od warunku logicznego boolean
+    :param boolean:
+    :return: string:
+    """
+    nie = ""
+    if not boolean:
+        nie = "nie"
+    return f"Podana liczba {nie} jest w zakresie"
 def sprawdzenie_1(liczba,lista):
     """
+    Sprawdza czy podana liczba znajduje sie w podanej kolekcji liczb(lista)
     :param liczba:
     :param lista:
     :return:
     """
-    if liczba in lista:
-        print('Liczba znajduje sie na liscie')
-    else:
-        print('Liczba nie znajduje sie na liscie')
+    return liczba in lista
 
+def sprawdzenie_2(liczba,range_of_numbers):
+    """
+
+    :param liczba:
+    :param range_of_numbers:
+    :return: boolean:
+    """
+    return liczba in range_of_numbers
 if __name__ == '__main__':
-    sprawdzenie_1(1,[1,2,3])
-    sprawdzenie_1(1,range(2,8))
+    print("Sprawdzenie pierwszej funkcji czy liczba jest w kolekcji")
+    print(zdanie(sprawdzenie_1(1,[1,2,3])))
+    print(zdanie(sprawdzenie_1(4,[1,2])))
+    print()
+    print("Sprawdzenie drugiej funkcji czy liczba jest w zakresie liczb")
+    print(zdanie(sprawdzenie_2(3,range(2,8))))
+    print(zdanie(sprawdzenie_2(8,(1,7))))
+
