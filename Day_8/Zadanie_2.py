d6d128489879a6a185075a931ac8ab73984445c3
Szegerege12 <mrszegerege@gmail.com>
2018-10-25 12:39:44 +0200

    z2 day 8 done

--- Day_8/Zadanie_2.py
+++ Day_8/Zadanie_2.py
@@ -11,3 +11,20 @@ Następnie otwórz plik i wypisz jego zawartość na ekran.
 Podpowiedź:
 Użyj pliku db_engine_before_refactoring jako wzoru
 """
+lista = []
+import csv
+
+decyzja = str(input("Witaj w programie. Jesli chcesz wpisac imiona nacisnij klawisz 1, jezeli chcesz zakonczyc nacisnij klawisz 2: "))
+while decyzja != '2':
+    if decyzja == '1':
+        imie = str(input('Podaj imie: '))
+        nazwisko = str(input('Nazwisko: '))
+        data_ur = str(input('Rok urodzenia: '))
+        with open('dane_zadanie_3.csv', 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter=",")
+            writer.writerow([imie, nazwisko, data_ur])
+    else:
+        print('Podales zle polecenie')
+
+    decyzja = str(input("Podaj nowe polecenie: "))
+
